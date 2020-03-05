dc414bc451b80991c3dc2600bd162e9b8ad9d861
Nebojša Ćirić <nciric@gmail.com>
2020-03-04 17:48:24 -0800

    Updating test.

--- python/group_f_c_unittest.py
+++ python/group_f_c_unittest.py
@@ -4,11 +4,15 @@ import unittest
 
 class TestGroupFCInflection(unittest.TestCase):
 
-    def test_group_f_c_thing(self):
+    def test_group_f_c_1(self):
         self.assertEqual(["ствар", "ствари", "ствари", "ствар", "ствари", "ствари", "ствари",
                           "ствари", "ствари", "стварима", "ствари", "ствари", "стварима", "стварима"],
                          f_c.inflect("ствар"))
 
+    def test_group_f_c_2(self):
+        self.assertEqual(["чађ", "чађи", "чађи", "чађ", "чађи", "чађи", "чађи",
+                          "чађи", "чађи", "чађима", "чађи", "чађи", "чађима", "чађима"],
+                         f_c.inflect("чађ"))
 
 if __name__ == '__main__':
     unittest.main()
