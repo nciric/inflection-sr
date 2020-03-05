7707ed75a9bac4256f23b907dc0b125d5ae2a5f6
Nebojša Ćirić <nciric@gmail.com>
2020-03-04 17:51:37 -0800

    Fix grammar.

--- python/group_f_c.py
+++ python/group_f_c.py
@@ -1,6 +1,6 @@
 """ Implements declination group 4. From documentation:
 Именице женског рода које се у номинативу једнине завршавају наставком -∅ (тј. имају видљив сугласнички завршетак) мењају се по четвртој именичкој врсти.
-TODO(nciric): Having a adjective in front changes declination rules in some cases. Handle that case.
+TODO(nciric): Having an adjective in front changes declination rules in some cases. Handle that case.
 """
 
 EXTENSIONS = ["", "и", "и", "", "и", "и", "и",
