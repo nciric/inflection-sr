307ce28514236a88d696a53d5ecf10227c9adfe3
Nebojša Ćirić <nciric@gmail.com>
2020-03-25 17:29:44 -0700

    Adding country handling for group 3.

--- python/group_mfn_a.py
+++ python/group_mfn_a.py
@@ -15,9 +15,15 @@ def inflect(singular):
     is_multi = utils.is_multisyllabic(singular)
     alt_extensions = EXTENSIONS
     # Fixing vocative, singular exceptions.
-    if singular.endswith('ица') and is_multi:
+    if is_country_exception(singular):
+        pass
+    elif singular.endswith('ица') and is_multi:
         alt_extensions[defs.Cases.S_VOCATIVE] = 'е'
-    if (singular[0:1].isupper() and is_multi) or is_exception(singular):
+    # Countries, made from adjective
+    elif singular[0:1].isupper() and singular.endswith(('ска', 'чка', 'шка')):
+        alt_extensions[defs.Cases.S_VOCATIVE] = 'а'
+    # Proper name, multisyllabic or an exception.
+    elif (singular[0:1].isupper() and is_multi) or is_exception(singular):
         alt_extensions[defs.Cases.S_VOCATIVE] = 'а'
     # Fixing genitive, plural exceptions.
     return [generate_case(singular, x, i) for (i, x) in enumerate(alt_extensions, 0)]
@@ -40,13 +46,25 @@ def generate_case(singular, extension, case):
 
     # Case of "nepostojano a": devojka -> devoj_a_ka.
     singular = utils.maybe_insert_nepostojano_a(singular)
-    
+
     return singular + extension
+
+
+def is_country_exception(singular):
+    """ Some countries inflect properly (so no exceptions), but fall into:
+        multisyllabic, personal name case, so we need to make them exceptions."""
+    EXCEPTIONS = ['Аустрија', 'Албанија', 'Аустралија', 'Ангола', 'Аргентина', 'Јерменија',
+                  'Белорусија', 'Белгија', 'Боливија', 'Боцвана', 'Камбоџа', 'Канада', 'Костарика', 'Еритреја', 'Естонија',
+                  'Етиопија', 'Гамбија', 'Грузија', 'Гренада', 'Гватемала', 'Гвинеја', 'Индија', 'Индонезија', 'Италија', 'Јамајка',
+                  'Кенија', 'Кореја', 'Летонија', 'Либерија', 'Либија', 'Литванија', 'Малезија', 'Мауританија', 'Микронезија',
+                  'Молдавија', 'Монголија', 'Намибија', 'Холандија', 'Никарагва', 'Нигерија', 'Македонија', 'Палестина',
+                  'Панама', 'Португалија', 'Румунија', 'Русија', 'Руанда', 'Самоа', 'Србија', 'Словенија', 'Сомалија', 'Шпанија',
+                  'Сирија', 'Танзанија', 'Уганда', 'Украјина', 'Венецуела', 'Замбија']
+    return singular in EXCEPTIONS
 
 
 def is_exception(singular):
     """ For many of these, the only change is vocative, singular that goes from o to a."""
     EXCEPTIONS = ['Бранка', 'Ана', 'Уна',
-                  'Француска', 'Енглеска', 'Немачка',
                   'мама', 'тата', 'баба', 'деда', 'стрина', 'тетка']
     return singular in EXCEPTIONS
